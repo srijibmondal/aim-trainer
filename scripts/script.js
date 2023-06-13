ef0dbf917d6ca7130c6b8eb1cb7271a6d583d154
srijib <kabird217@gmail.com>
2023-06-13 20:18:31 +0530

    compliting single-aim trainer

--- scripts/script.js
+++ scripts/script.js
@@ -14,9 +14,13 @@ const hitsEl = document.querySelector("#hits");
 const accuracyEl = document.querySelector("#accuracy");
 const lives = document.querySelectorAll(".live");
 const gameOverTxt = document.querySelector(".game__over");
+const overHitsAccuracy = document.querySelector(".over__hits__accuracy");
+const overHits = document.querySelector("#hits__over");
+const overAccuracy = document.querySelector("#accuracy__over");
+const hitsContaienr = document.querySelector(".hits")
+const accuracyContaienr = document.querySelector(".accuracy")
 
 let playing = false;
-// let aimSize = 3;
 let difficulty = 1;
 let size = 3;
 let hits = 0;
@@ -24,13 +28,37 @@ let accuracy = 0;
 let missed = 0;
 
 startBtn.addEventListener("click", () => {
-  // console.log("hola")
   startBtn.classList.add("dn");
   difficultyOptions.classList.add("dn");
   aimSizeOptions.classList.add("dn");
   advice.classList.add("dn");
   restartBtns[1].classList.remove("dn");
   startGame();
+});
+
+restartBtns[0].addEventListener("click", () => {
+  // startBtn.classList.add("dn");
+  hits = 0;
+  missed = 0;
+  accuracy = 0;
+  accuracyEl.innerHTML = "0%";
+  hitsEl.innerHTML = "0";
+  lives[0].classList.remove("dead");
+  lives[1].classList.remove("dead");
+  lives[2].classList.remove("dead");
+  startGame();
+  difficultyOptions.classList.add("dn");
+  restartBtns[1].classList.remove("dn");
+  gameOverTxt.classList.add("dn");
+  restartBtns[0].classList.add("dn");
+  aimSizeOptions.classList.add("dn");
+  overHitsAccuracy.classList.add("dn");
+  hitsContaienr.classList.remove("dn")
+  accuracyContaienr.classList.remove("dn")
+
+  // advice.classList.add("dn");
+
+  console.log("restart");
 });
 
 function startGame() {
@@ -41,7 +69,7 @@ function startGame() {
 function createRandomCircle() {
   checkLives();
   if (!playing) {
-    console.log("not playing");
+    // console.log("not playing");
   } else {
     const circle = document.createElement("div");
     const { width, height } = gameBoard.getBoundingClientRect();
@@ -50,17 +78,9 @@ function createRandomCircle() {
     circle.classList.add("circle");
     calculateDifficulty();
     calculateAimSize();
-    //   circle.style.aspectRatio = "1/1";
-    //   circle.style.position = "absolute";
     circle.style.top = `${y}px`;
     circle.style.left = `${x}px`;
-    //   circle.style.background = "#00c3ff";
-    // circle.style.zIndex = "100"
     gameBoard.append(circle);
-    //   console.log("circle created");
-    //   console.log(x, y);
-    //   console.log(circle);
-    // console.log(width, height);
 
     function calculateAimSize() {
       if (size === 1) {
@@ -98,19 +118,30 @@ function createRandomCircle() {
       decreaseLive();
       createRandomCircle();
     });
-    //   circle.addEventListener("click", () => {
-    //     console.log("aimed");
-    //     circle.remove();
-    //     createRandomCircle();
-    //     hits++;
-    //     hitsEl.innerHTML = hits;
-    //   });
+    restartBtns[1].addEventListener("click", () => {
+      circle.remove();
+      hits = 0;
+      missed = 0;
+      accuracy = 0;
+      playing = false;
+      advice.classList.remove("dn");
+      startBtn.classList.remove("dn");
+      difficultyOptions.classList.remove("dn");
+      aimSizeOptions.classList.remove("dn");
+      restartBtns[1].classList.add("dn");
+      lives[0].classList.remove("dead");
+      lives[1].classList.remove("dead");
+      lives[2].classList.remove("dead");
+      gameOverTxt.classList.add("dn");
+      restartBtns[0].classList.add("dn");
+      hitsEl.innerHTML = "0";
+      accuracyEl.innerHTML = "0%";
+    });
   }
 }
 
 gameBoard.addEventListener("click", (e) => {
   if (e.target.classList.contains("circle")) {
-    // console.log("clicked")
     e.target.remove();
     createRandomCircle();
     hits++;
@@ -178,6 +209,13 @@ function finishGame() {
   gameOverTxt.classList.remove("dn");
   restartBtns[0].classList.remove("dn");
   restartBtns[1].classList.add("dn");
+  aimSizeOptions.classList.remove("dn");
+  difficultyOptions.classList.remove("dn");
+  overAccuracy.innerHTML = `${accuracy}%`;
+  overHits.innerHTML = hits;
+  overHitsAccuracy.classList.remove("dn");
+  hitsContaienr.classList.add("dn")
+  accuracyContaienr.classList.add("dn")
 }
 
 function decreaseLive() {
@@ -197,23 +235,23 @@ function decreaseLive() {
   }
 }
 
-restartBtns.forEach((e) => {
-  e.addEventListener("click", () => {
-    hits = 0;
-    missed = 0;
-    accuracy = 0;
-    playing = false;
-    advice.classList.remove("dn");
-    startBtn.classList.remove("dn");
-    difficultyOptions.classList.remove("dn");
-    aimSizeOptions.classList.remove("dn");
-    restartBtns[1].classList.add("dn");
-    lives[0].classList.remove("dead");
-    lives[1].classList.remove("dead");
-    lives[2].classList.remove("dead");
-    gameOverTxt.classList.add("dn");
-    restartBtns[0].classList.add("dn");
-    hitsEl.innerHTML = "0";
-    accuracyEl.innerHTML = "0%";
-  });
-});
+// restartBtns.forEach((e) => {
+//   e.addEventListener("click", () => {
+//     hits = 0;
+//     missed = 0;
+//     accuracy = 0;
+//     playing = false;
+//     advice.classList.remove("dn");
+//     startBtn.classList.remove("dn");
+//     difficultyOptions.classList.remove("dn");
+//     aimSizeOptions.classList.remove("dn");
+//     restartBtns[1].classList.add("dn");
+//     lives[0].classList.remove("dead");
+//     lives[1].classList.remove("dead");
+//     lives[2].classList.remove("dead");
+//     gameOverTxt.classList.add("dn");
+//     restartBtns[0].classList.add("dn");
+//     hitsEl.innerHTML = "0";
+//     accuracyEl.innerHTML = "0%";
+//   });
+// });
